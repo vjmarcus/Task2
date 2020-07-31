c22634be1c2bc4cf97d672a34df187f9f97d0de7
vjmarcus <vjmarcus@list.ru>
2020-07-31 17:37:14 +0300

    Add buttons

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.task2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton playImageButton;
+    private ImageButton pauseImageButton;
+    private ImageButton stopImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setOnClickListener();
+        getResources().openRawResource(R.raw.intergalactic);
+    }
+
+    private void setOnClickListener() {
+        playImageButton.setOnClickListener(this);
+        pauseImageButton.setOnClickListener(this);
+        stopImageButton.setOnClickListener(this);
+    }
+
+    private void init() {
+        playImageButton = findViewById(R.id.playImageButton);
+        pauseImageButton = findViewById(R.id.pauseImageButton);
+        stopImageButton = findViewById(R.id.stopImageButton);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.playImageButton:
+                break;
+            case R.id.pauseImageButton:
+                break;
+            case R.id.stopImageButton:
+                break;
+
+        }
     }
 }
